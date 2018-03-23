66bf52d03b20010707bf38cdec4698d1bf8dac36
thakorraj <thakorraj80@yahoo.com>
2018-03-23 20:43:29 +0530

    Reset password and edit profile done

--- src/app/login/user.service.ts
+++ src/app/login/user.service.ts
@@ -8,6 +8,7 @@ export class UserService {
   public url: string = "http://localhost:3000/users/";
   public url1:string="http://localhost:3000/login/";
   public url2: string = "http://localhost:3000/uemail/";
+  public url3:string="http://localhost:3000/userpassword/"
   constructor(public _http: HttpClient) { }
   content: string = "Content-Type";
   header: string = "application/json";
@@ -28,6 +29,18 @@ export class UserService {
       getUserByEmail(id)
       {
         return this._http.get(this.url2+id);
+      }
+
+      editUser(id,item){
+        let body = JSON.stringify(item);
+        return this._http.put(this.url+id, body, { headers: new HttpHeaders().set(this.content, this.header) });
+     
+      }
+
+      Resetpassword(id,item)
+      {
+        let body = JSON.stringify(item);
+        return this._http.put(this.url3+id, body, { headers: new HttpHeaders().set(this.content, this.header) });
       }
 
 }
--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router,ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs/Rx';
+import { User } from '../login/userc';
+import { UserService } from "../login/user.service";
 
 @Component({
   selector: 'app-user-profile',
@@ -7,9 +11,81 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor() { }
+  public _subscription:Subscription;
+  email:string="";
+  name1:string="";
+  gender:string="";
+  mobile1:string="";
+  address1:string="";
+  bod1:string="";
+  img:string="";
+  email1:string="";
+
+  pass:string="";
+  pass1:string="";
+  pass2:string="";
+  password:string;
+  
+
+  constructor(public _router:Router,public _activatedRoute:ActivatedRoute,public _data:UserService) { }
 
   ngOnInit() {
+    this._subscription=this._activatedRoute.params.subscribe(
+      (para:any)=>{
+       //   this.email=para["email_id"];
+        this.email=localStorage.getItem('Email');
+          this.email1=this.email;
+          console.log(this.email1);
+         
+      }
+  );
+
+  this._data.getUserByEmail(this.email).subscribe(
+    (data:User[])=>{
+      this.name1=data[0].user_name;
+      this.gender=data[0].user_gender;
+      this.address1=data[0].user_address;
+      this.mobile1=data[0].user_mobile_no;
+      this.bod1=data[0].user_DO_B;
+      this.img=data[0].user_photo;
+      this.password = data[0].user_password;
+      console.log(data);
+    }
+  );
+
   }
+
+  onUpdate(){
+    let user=new User(this.email,'',this.name1,this.address1,this.bod1,this.gender,this.img,this.mobile1);
+    this._data.editUser(this.email,user).subscribe(
+      (data:User[])=>{
+        this._router.navigate(['/user']);
+        console.log(data);
+        console.log("Yes")
+        alert("Changes Saved");
+      }
+    );
+  }
+
+  Changepassword(){
+    if(this.pass1==this.password)
+    {
+      if(this.pass==this.pass2)
+      {
+    let user=new User(this.email,this.pass,'','','','','','');
+    this._data.Resetpassword(this.email,user).subscribe(
+      ()=>{
+        //this._router.navigate(['/Edittraveller']);
+        console.log("Yess");
+        alert("Password Changed");
+      }
+    );
+  }
+}
+  else
+  {
+    alert("Please enter valid password");
+  }
+}
 
 }
